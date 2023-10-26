ea38a0fd1d92998669665201a10d250f8fbf5f1d
i.gorbacz <i.gorbacz@omtech.pl>
2023-10-26 14:50:49 +0200

    Written tests for logger

--- code-reviews/src/logger/index.test.ts
+++ code-reviews/src/logger/index.test.ts
@@ -1,11 +1,25 @@
-import logger from './index'
-describe('logger', () => {
-   it('should log the provided text with timestamp', () => {
-      const spy = jest.spyOn(console, 'log');
-      const text = 'Hello, world!';
-      const mockDate = new Date(2023, 9, 18, 12, 0, 0);
-      jest.spyOn(global, 'Date').mockImplementation(() => mockDate);
-      expect(spy).toHaveBeenCalledWith(`${mockDate} - ${text}`);
-      spy.mockRestore();
+import logger from '../logger'
+
+describe('Logger Tests', () => {
+   it('should return current date and time in ISO format', () => {
+      const dateTime = logger.getCurrentDateTime();
+      const regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+      expect(dateTime).toMatch(regex);
+   });
+
+   it('should log an info message', () => {
+      const spyConsoleLog = jest.spyOn(console, 'log').mockImplementation(() => {});
+      logger.info('This is an info message');
+      const dateTime = logger.getCurrentDateTime();
+      expect(spyConsoleLog).toHaveBeenCalledWith(`[${dateTime}][INFO] This is an info message`);
+      spyConsoleLog.mockRestore();
+   });
+
+   it('should log an error message', () => {
+      const spyConsoleError = jest.spyOn(console, 'error').mockImplementation(() => {});
+      logger.error('This is an error message');
+      const dateTime = logger.getCurrentDateTime();
+      expect(spyConsoleError).toHaveBeenCalledWith(`[${dateTime}][ERROR] This is an error message`);
+      spyConsoleError.mockRestore();
    });
 });
